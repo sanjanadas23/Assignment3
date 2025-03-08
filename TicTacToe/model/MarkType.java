3620bdbf70a71d03be7d5aedbfb1a81688873f5b
Sanjana Das <sanjanad845@gmail.com>
2025-03-08 13:40:34 +0530

    Add files via upload

---
+++ TicTacToe/model/MarkType.java
@@ -0,0 +1,6 @@
+package TicTacToe.model;
+
+public enum MarkType {
+	E, X, O 
+
+}
---
+++ TicTacToe/test/TictacToeMain.java
@@ -0,0 +1,11 @@
+package TicTacToe.test;
+
+import TicTacToe.model.TicTacToe;
+
+public class TictacToeMain {
+	public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.playGame();
+    }
+
+}
